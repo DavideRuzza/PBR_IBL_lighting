d3ba94378d4d380dd3a4fce6e2c4945e95582a39
DavideRuzza <davideruzza.98@gmail.com>
2023-08-30 22:18:15 +0200

    PBR working

--- PBR.py
+++ PBR.py
@@ -2,7 +2,7 @@ import moderngl_window as mglw
 import moderngl as mgl
 import numpy as np
 from camera import OrbitCamera
-from moderngl_window.geometry import sphere
+from moderngl_window.geometry import sphere, cube
 from pyrr import Matrix44 as m44
 
 class MainWin(OrbitCamera):
@@ -16,38 +16,65 @@ class MainWin(OrbitCamera):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.N_inst = 40
+        self.N_inst = 400
         self.simple3d = self.load_program("shader/simple3d.glsl")
+        self.pbr_prog = self.load_program("shader/PBR.glsl")
 
         self.proj_mat = m44.perspective_projection(40, self.aspect_ratio, 0.1, 100)
-
-        x_pos = (np.arange(self.N_inst)-(self.N_inst-1)/2)*0.2
-        y_pos = (np.arange(self.N_inst)-(self.N_inst-1)/2)*0.2
-
-        x, y = np.meshgrid(x_pos, y_pos)
-        z = np.sin(x)*np.sin(y)
-        pos =  np.stack([x, y, z], axis=2)
-        self.pos = pos.reshape((pos.shape[0]*pos.shape[1], pos.shape[2]))
-
-
-        self.sph = sphere(0.1, 20, 32, True, True)
-
-        self.inst_data = self.ctx.buffer(reserve=4*3*self.N_inst*self.N_inst)
-        self.sph.buffer(self.inst_data, "3f4/i", "in_offset")
+        self.sph = sphere(0.4)
         
+        self.pos = np.array([
+            [0, 0, 0],
+            [1, 0, 0],
+            [2, 0, 0]
+        ], dtype='f4')
+
+        self.sph_col = np.array([
+            [0.00, 0.33, 1.00],
+            [0.68, 0.92, 0.16],
+            [0.73, 0.25, 0.86]
+        ], dtype='f4')
+
+
+        self.materials = np.array([
+            #albedo              metallic   rough   ao
+            [0.00, 0.33, 1.00,    0.0      , 0.1   , 0.01 ],
+            [0.68, 0.92, 0.16,    0.2      , 0.3   , 1.0 ],
+            [0.73, 0.25, 0.86,    0.8      , 0.1   , 0.02 ]
+        ], dtype='f4')
+
+        self.N_inst = 3
+
+        self.sph.buffer(self.pos, "3f/i", "in_offset")
+        # self.sph.buffer(self.sph_col, "3f/i", "in_albedo")
+        self.sph.buffer(self.materials, "3f f f f/i", ["in_albedo", "in_metallness", "in_roughness", "in_ao"])
+
         
-        # print(x_pos)
-    
+        self.light_pos = np.array([
+            [2, 2, 2],
+            [-2, 2, 2],
+            [2, -2, 2],
+            [2, 2, -2],
+        ], dtype='f4')
+        
+        self.light_col = np.array([
+            [1., 1., 1.],
+            [1., 1., 1.],
+            [1., 1., 1.],
+            [1., 1., 1.]
+        ], dtype='f4')
 
         self.ctx.enable(mgl.DEPTH_TEST)
     
     def render(self, t, dt):
         self.ctx.clear()
         
-        self.inst_data.write((self.pos*[1, 1, np.sin(2*t)]).astype('f4'))
         mvp = self.proj_mat*self.view_mat
-        self.simple3d["mvp"].write(mvp.astype('f4'))
-        self.sph.render(self.simple3d, instances=self.N_inst*self.N_inst)
+        self.pbr_prog["mvp"].write(mvp.astype('f4'))
+        self.pbr_prog["lightPositions"].write(self.light_pos*2)
+        self.pbr_prog["camPos"].write(self.eye)
+        self.pbr_prog["lightColors"].write(self.light_col)
+        self.sph.render(self.pbr_prog, instances=self.N_inst)
         # self.vao_inst.render(instances=self.N_inst)
 
 MainWin.run()
---
+++ resources/shader/PBR.glsl
@@ -0,0 +1,154 @@
+#version 460
+
+#ifdef VERTEX_SHADER
+
+uniform mat4 mvp;
+
+in vec3 in_position;
+in vec2 in_texcoord_0;
+in vec3 in_normal;
+
+in vec3 in_offset;
+
+// out vec2 uv;
+// out vec3 N;
+
+// // material parameters
+in vec3  in_albedo;
+in float in_metallness;
+in float in_roughness;
+in float in_ao;
+
+out vec3  albedo;
+out float metallic;
+out float roughness;
+out float ao;
+
+out vec3 WorldPos;
+out vec3 Normal;
+
+void main(){
+    gl_Position = mvp*vec4(in_position+in_offset, 1.0);
+    // uv = in_texcoord_0;
+    albedo = in_albedo;
+    metallic = in_metallness;
+    roughness = in_roughness;
+    ao = in_ao;
+    Normal = in_normal;
+    WorldPos = in_position+in_offset;
+}
+
+
+#elif FRAGMENT_SHADER
+
+layout(location=0) out vec4 fragColor;
+
+// material parameters
+in vec3  albedo;
+in float metallic;
+in float roughness;
+in float ao;
+
+in vec3 Normal;
+in vec3 WorldPos;
+
+// // lights
+uniform vec3 lightPositions[4];
+uniform vec3 lightColors[4];
+
+uniform vec3 camPos;
+
+const float PI = 3.14159265359;
+  
+float DistributionGGX(vec3 N, vec3 H, float roughness);
+float GeometrySchlickGGX(float NdotV, float roughness);
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness);
+vec3 fresnelSchlick(float cosTheta, vec3 F0);
+
+vec3 fresnelSchlick(float cosTheta, vec3 F0)
+{
+    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
+} 
+
+float DistributionGGX(vec3 N, vec3 H, float roughness)
+{
+    float a      = roughness*roughness;
+    float a2     = a*a;
+    float NdotH  = max(dot(N, H), 0.0);
+    float NdotH2 = NdotH*NdotH;
+	
+    float num   = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = PI * denom * denom;
+	
+    return num / denom;
+}
+
+float GeometrySchlickGGX(float NdotV, float roughness)
+{
+    float r = (roughness + 1.0);
+    float k = (r*r) / 8.0;
+
+    float num   = NdotV;
+    float denom = NdotV * (1.0 - k) + k;
+	
+    return num / denom;
+}
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
+{
+    float NdotV = max(dot(N, V), 0.0);
+    float NdotL = max(dot(N, L), 0.0);
+    float ggx2  = GeometrySchlickGGX(NdotV, roughness);
+    float ggx1  = GeometrySchlickGGX(NdotL, roughness);
+	
+    return ggx1 * ggx2;
+}
+
+void main()
+{		
+    vec3 N = normalize(Normal);
+    vec3 V = normalize(camPos - WorldPos);
+
+    vec3 F0 = vec3(0.04); 
+    F0 = mix(F0, albedo, metallic);
+	           
+    // reflectance equation
+    vec3 Lo = vec3(0.0);
+    int i = 1;
+    for(int i = 0; i < 4; ++i) 
+    {
+        // calculate per-light radiance
+        vec3 L = normalize(lightPositions[i] - WorldPos);
+        vec3 H = normalize(V + L);
+        float distance    = length(lightPositions[i] - WorldPos);
+        float attenuation = 1.0; // / (distance * distance);
+        vec3 radiance     = lightColors[i] * attenuation;        
+        
+        // cook-torrance brdf
+        float NDF = DistributionGGX(N, H, roughness);        
+        float G   = GeometrySmith(N, V, L, roughness);      
+        vec3 F    = fresnelSchlick(max(dot(H, V), 0.0), F0);       
+        
+        vec3 kS = F;
+        vec3 kD = vec3(1.0) - kS;
+        kD *= 1.0 - metallic;	  
+        
+        vec3 numerator    = NDF * G * F;
+        float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.0001;
+        vec3 specular     = numerator / denominator;  
+            
+        // add to outgoing radiance Lo
+        float NdotL = max(dot(N, L), 0.0);                
+        Lo += (kD * albedo / PI + specular) * radiance * NdotL; 
+    }   
+  
+    vec3 ambient = vec3(0.03) * albedo * ao;
+    vec3 color = ambient + Lo;
+	
+    color = color / (color + vec3(1.0));
+    color = pow(color, vec3(1.0/2.2));  
+   
+    fragColor = vec4(color, 1.0);
+}  
+
+#endif
